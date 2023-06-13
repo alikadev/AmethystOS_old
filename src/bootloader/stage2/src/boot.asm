8b2f43c267523e850cfc780c9f73e46242352041
alikadev <dev@alikadev.com>
2023-06-13 23:30:33 +0200

    Jump to protected mode

--- src/bootloader/stage2/src/boot.asm
+++ src/bootloader/stage2/src/boot.asm
@@ -6,42 +6,47 @@
 ; TODO: Adding a Load Menu to choose what kernel to run
 
 entry:
-	mov  	si,		s_welcome
-	call puts
+	cli
+	lgdt 	[GDT_Descriptor]
+	; Change last bit of CR0 to 1
+	mov 	eax, 		cr0
+	or 		eax,		1
+	mov 	cr0,		eax
+	; far jump to protected mode
+	jmp 	CODE_SEGMENT:pmode 
+
+
+[bits 32]
+pmode:
+	mov  [0xB8000], word 0x8F00|'H'
+	mov  [0xB8002], word 0x8F00|'e'
+	mov  [0xB8004], word 0x8F00|'l'
+	mov  [0xB8006], word 0x8F00|'l'
+	mov  [0xB8008], word 0x8F00|'o'
 	cli
 	hlt
 
-;
-; Print AL in to the screen, 0x0A is interpreted as 0x0D, 0x0A
-putc:
-	push  	ax 										; Save AX
-	mov 	ah, 	0x0E					 		; Write character mode
-	cmp 	al, 	0x0A						 	; Check if AL is LF
-	je  	.line 									;  > Draw new line
-.end: 
-	int 	0x10									; Interrupt Video
-	pop 	ax 										; Restore AX
-	ret
-.line:
-	mov 	al,		0x0D 							; Set AL to CR
-	int 	0x10
-	mov 	al,		0x0A 							; Set AL to LF
-	jmp 	.end 	
+GDT:
+.null_segment: 	dd 0, 0 
 
-;
-; Print the CSTR referenced by SI
-puts:
-	push 	si 										; Save SI
-	push 	ax 										; Save AX
-.fetch:
-	lodsb 											; Load next char from SI in AL
-	or  	al, 	al 								; Check if AL is 0
-	jz  	.end 									;  > End
-	call 	putc 									; Print char
-	jmp 	.fetch 									; Fetch next
+.code_segment: 	dw 0xFFFF 		; Limit 16 bits
+				dw 0 			; base 16bits
+				db 0 			; base 8 bit = 24bit
+				db 0b10011010 	; present,privilege,type,Type flags
+				db 0b11001111 	; Other flags + Limit (last 4 bits)
+				db 0 			; Last 8 bits of the base
+
+.data_segment:	dw 0xFFFF 		; Limit
+				dw 0 			; Base 16 bits
+				db 0 			; Base 8 bits = 24 bits
+				db 0b10010010 	; present,privilege,type,Type flags
+				db 0b11001111 	; Pther flags + limit (last 4 bits)
+				db 0 			; Last 8 bit of the base
 .end:
-	pop 	ax 										; Restore AX
-	pop 	si 										; Restore SI
-	ret
 
-s_welcome: db	"Welcome in the second stage", 0x0A, 0
+GDT_Descriptor:
+	dw GDT.end - GDT - 1 		; Size of the GDT
+	dd GDT 						; Address of the GDT
+
+CODE_SEGMENT equ GDT.code_segment - GDT
+DATA_SEGMENT equ GDT.data_segment - GDT
