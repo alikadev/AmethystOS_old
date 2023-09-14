1be26ccf65f805a88db697a8d84c6fb5d08d18cc
alikadev <dev@alikadev.com>
2023-09-14 21:24:29 +0200

    Remove the old app. Will be replaced later...

--- src/kernel/include/app.h
+++
@@ -1,6 +0,0 @@
-#ifndef __KERNEL_APP_H_
-#define __KERNEL_APP_H_
-
-void kernel_app(void);
-
-#endif
--- src/kernel/src/_start.c
+++ src/kernel/src/_start.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-#include <app.h>
+#include <stdlib.h>
+
+#define HEAP_START ((void*)0x1000000)
+#define HEAP_SIZE  ((size_t)0x1000000)
 
 void _start(void)
 {
 	clear();
 	printf("Welcome in %VAmethystOS%V!\n", 0x0C, __DEFAULT_VIDEO_COLOR);
-	kernel_app();
+	alloc_init(HEAP_START, HEAP_SIZE);
+	void *first = malloc(1);
+	free(first);
+	malloc(HEAP_SIZE - 1);
+	void *last = malloc(1);
+	printf("0x%x : 0x%x\n", first, last);
+	while(1)
+		;;
 }
