3a285114d7ab0f4d015de23f013897dc73ce9057
alikadev <dev@alikadev.com>
2023-10-12 18:53:59 +0200

    Add the _start initialization...

--- src/kernel/src/_start.c
+++ src/kernel/src/_start.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/io.h>
+#include <kernel/idt.h>
+#include <kernel/syscalls.h>
 
 #define HEAP_START ((void*)0x1000000)
 #define HEAP_SIZE  ((size_t)0x1000000)
 
+void testsyscalls(void);
+
 void _start(void)
 {
 	clear();
-	printf("Welcome in %VAmethystOS%V!\n", 0x0C, __DEFAULT_VIDEO_COLOR);
+
+	// Init IDT
+	idt_init();
+
+	// Attach the syscall interrupt code
+	idt_set_descriptor(
+			0xAA, dispatch, 
+			IDT_PRESENT 
+			| IDT_GATE_INT32 
+			| IDT_RING_KERNEL);
+
+	// Initialize the allocator
 	alloc_init(HEAP_START, HEAP_SIZE);
-	void *first = malloc(1);
-	free(first);
-	malloc(HEAP_SIZE - 1);
-	void *last = malloc(1);
-	printf("0x%x : 0x%x\n", first, last);
-	while(1)
-		;;
+
+	halt();
 }
