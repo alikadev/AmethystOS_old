5f4f16f0c1a4359be3ee0808cf67e12b9071e6d8
alikadev <dev@alikadev.com>
2023-10-12 21:08:52 +0200

    Change video mode to VGA and add a font type

--- src/kernel/include/stdio.h
+++ src/kernel/include/stdio.h
@@ -2,10 +2,15 @@
 #define __IO_H_
 
 #include <stdint.h>
+#include <sys/font.h>
+#include <kernel/vga.h>
 
 #define __DEFAULT_VIDEO_COLOR (uint8_t)(0x05)
 
-void putc(const char c);
+void set_font(const font_t *pFont);
+void set_fg_color(const vga_color_t fgColor);
+void set_bg_color(const vga_color_t bgColor);
+void putchar(const int c);
 void setcol(const uint8_t color);
 void puts(const char *s);
 void printf(const char *format, ...);
--- src/kernel/src/_start.c
+++ src/kernel/src/_start.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/io.h>
+#include <sys/font.h>
 #include <kernel/idt.h>
 #include <kernel/syscalls.h>
 
@@ -9,9 +10,12 @@
 
 void testsyscalls(void);
 
+extern font_t font_6x8;
+
 void _start(void)
 {
 	clear();
+	set_font(&font_6x8);
 
 	// Init IDT
 	idt_init();
@@ -25,6 +29,21 @@ void _start(void)
 
 	// Initialize the allocator
 	alloc_init(HEAP_START, HEAP_SIZE);
+	printf("VGA video test:\n");
+	printf("  -0-1-2-3-4-5-6-7-8-9-A-B-C-D-E-F\n");
+	printf("0-\n1-\n2-\n3-\n4-\n5-\n6-\n7-\n8-\n9-\nA-\nB-\nC-\nD-\nE-\nF-\n");
+	for (int yoff = 0; yoff < 0x10; ++yoff)
+	{
+		for (int xoff = 0; xoff < 0x10; ++xoff)
+		{
+			vga_draw_rect(
+					font_6x8.width * 2 * (xoff+1),
+					font_6x8.height    * (yoff+2),
+					font_6x8.width  * 2,
+					font_6x8.height,
+					(yoff << 4) | xoff);
+		}
+	}
 
 	halt();
 }
