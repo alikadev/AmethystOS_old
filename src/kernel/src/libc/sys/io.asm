4f134747e62f83fb628b0884787217cf3e10c542
alikadev <dev@alikadev.com>
2023-10-15 17:07:39 +0200

    Replace the assembly by __asm__ in the in/out and add inl and outl

--- src/kernel/src/libc/sys/io.asm
+++ src/kernel/src/libc/sys/io.asm
@@ -1,43 +1,5 @@
 section .text
 
-global outb
-outb:
-	push ebp
-	mov ebp, esp
-	mov dx, [esp + 8]
-	mov al, [esp + 12]
-	out dx, al
-	pop ebp
-	ret
-
-global outw
-outw:
-	push ebp
-	mov ebp, esp
-	mov dx, [esp + 8]
-	mov ax, [esp + 12]
-	out dx, ax
-	pop ebp
-	ret
-
-global inb
-inb:
-	push ebp
-	mov ebp, esp
-	mov dx, [esp + 8]
-	in al, dx
-	pop ebp
-	ret
-
-global inw
-inw:
-	push ebp
-	mov ebp, esp
-	mov dx, [esp + 8]
-	in ax, dx
-	pop ebp
-	ret
-
 global halt
 halt:
 	cli
