a71d1b0bae641e18f16d830fcb6b3f514cde7a5b
alikadev <dev@alikadev.com>
2023-10-21 15:34:06 +0200

    Replace the old printf with the linux kernel printf

--- src/kernel/include/string.h
+++ src/kernel/include/string.h
@@ -2,8 +2,10 @@
 #define __STRING_H_
 
 #include <stdint.h>
+#include <stddef.h>
 
 uint32_t strlen(const char *str);
+size_t strnlen(const char *s, size_t maxlen);
 int strcmp(const char *s1, const char *s2);
 
 #endif
