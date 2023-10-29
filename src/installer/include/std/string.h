de7ae417189d88318576cbc56c0bedd76acc177c
alikadev <dev@alikadev.com>
2023-10-29 15:23:05 +0100

    Add strchr and strcpy

--- src/installer/include/std/string.h
+++ src/installer/include/std/string.h
@@ -8,6 +8,7 @@ uint32_t strlen(const char *str);
 size_t strnlen(const char *s, size_t maxlen);
 int strcmp(const char *s1, const char *s2);
 void *memcpy(void* dest, const void* src, size_t count);
-
+char *strchr(const char *p, int ch);
+char* strcpy(char* dest, const char* src);
 
 #endif
