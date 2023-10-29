bbab00d52535268540381157072ef7d92bea7ec0
alikadev <dev@alikadev.com>
2023-10-29 15:25:40 +0100

    Add a disk_t and fat12 FS support for it

---
+++ src/installer/src/sys/disk_internal.h
@@ -0,0 +1,8 @@
+#ifndef __SYS__DISK_INTERNAL_H_
+#define __SYS__DISK_INTERNAL_H_
+
+#include <stdint.h>
+
+disk_t *disk_create(uint8_t drive);
+
+#endif
---
+++ src/installer/src/sys/fs/fat12.c
@@ -0,0 +1,29 @@
+#include <sys/disk.h>
+#include <sys/fs/fat12.h>
+
+#include <string.h>
+
+void disk_create_fat12(
+	disk_t *disk, 
+	struct fs_fat12_header *header)
+{
+	disk->type = FS_FAT12;
+
+	// Copy the info from the header to the disk info
+	disk->info.fat12.bytes_per_sector = header->bytes_per_sector;
+	disk->info.fat12.sector_per_cluster = header->sector_per_cluster;
+	disk->info.fat12.reserved_sector_count = header->reserved_sector_count;
+	disk->info.fat12.fat_count = header->fat_count;
+	disk->info.fat12.max_root_entry_count = header->max_root_entry_count;
+	disk->info.fat12.sector_count = header->sector_count;
+	disk->info.fat12.sector_per_fat = header->sector_per_fat;
+	disk->info.fat12.sector_per_track = header->sector_per_track;
+	disk->info.fat12.head_count = header->head_count;
+	disk->info.fat12.boot_signature = header->boot_signature;
+	disk->info.fat12.volume_id = header->volume_id;
+
+	// Zero the first byte of the next field to copy it
+	// (!!!NEED TO CHANGE IT LATER!!!)
+	*header->fs_type = 0;
+	strcpy(disk->info.fat12.volume_label, header->volume_label);
+}
