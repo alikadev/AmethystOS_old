6592aad02305787f221be32d4923b2d975c0f655
alikadev <dev@alikadev.com>
2023-10-30 21:26:21 +0100

    Fix allocator unmerged chunk issue

--- src/installer/src/std/allocator.c
+++ src/installer/src/std/allocator.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#if ALLOC_DEBUG == 1
+#define alloc_debug(a ...) printf("ALC> " a);
+#else
+#define alloc_debug(...)
+#endif
+
 // CHUNKS
 #define CHUNK_QTY 0x4000
 typedef struct
@@ -29,6 +35,33 @@ int insert_chunk(_chunk_t *pChunk, _chunk_list_t list)
 	return 0;
 }
 
+int merge_remove_chunk(_chunk_t *pChunk, _chunk_list_t list)
+{
+	for (int i = 0; i < CHUNK_QTY; i++)
+	{
+		// Check if list is before the chunk
+		if (list[i].start + list[i].size == pChunk->start)
+		{
+			list[i].size += pChunk->size;
+			pChunk->size = 0;
+			pChunk->start = 0;
+			merge_remove_chunk(&list[i], list);
+			return 1;
+		}
+		// Check if list is after the chunk
+		if (pChunk->start + pChunk->size == list[i].start)
+		{
+			list[i].start = pChunk->start;
+			list[i].size = pChunk->size + list[i].size;
+			pChunk->size = 0;
+			pChunk->start = 0;
+			merge_remove_chunk(&list[i], list);
+			return 1;
+		}
+	}
+	return 0;
+}
+
 int merge_chunk(_chunk_t *pChunk, _chunk_list_t list)
 {
 	for (int i = 0; i < CHUNK_QTY; i++)
@@ -37,6 +70,7 @@ int merge_chunk(_chunk_t *pChunk, _chunk_list_t list)
 		if (list[i].start + list[i].size == pChunk->start)
 		{
 			list[i].size += pChunk->size;
+			merge_remove_chunk(&list[i], list);
 			return 1;
 		}
 		// Check if list is after the chunk
@@ -44,12 +78,12 @@ int merge_chunk(_chunk_t *pChunk, _chunk_list_t list)
 		{
 			list[i].start = pChunk->start;
 			list[i].size = pChunk->size + list[i].size;
+			merge_remove_chunk(&list[i], list);
 			return 1;
 		}
 	}
 
-	// Else try inserting
-	return insert_chunk(pChunk, list);
+	return 0;
 }
 
 int remove_chunk_by_size(_chunk_t *pChunk, _chunk_list_t list, const size_t size)
@@ -103,48 +137,78 @@ void alloc_init(void *start, size_t cap)
 	// cut and merge to create the entire alloc-free cycle!
 	freed[0].start = start;
 	freed[0].size = cap;
+
+#if ALLOC_DEBUG == 2
+	for (int i = 0; i < CHUNK_QTY; ++i)
+	{
+		if (freed[i].size == 0) break;
+		printf("[%p,%X]", freed[i].start, freed[i].size);
+	}
+	printf("\n");
+#endif
 }
 
 void free(void *ptr)
 {
 	// Ignore if try to free NULL
 	if (ptr == NULL) {
+		alloc_debug("W:ptr is NULL\n", ptr);
 		return;
 	}
 
 	_chunk_t chunk;
 	if (remove_chunk_by_address(&chunk, allocated, ptr) == 0)
 	{
-		// printf("Pointer %p is not allocated\n", ptr);
+		alloc_debug("W:ptr %p is not allocated\n", ptr);
 		return;
 	}
 
-	if (merge_chunk(&chunk, freed) == 0)
+	if (merge_chunk(&chunk, freed) == 0 && insert_chunk(&chunk, freed) == 0)
 	{
-		// printf("Fail to insert the chunk!\n");
+		alloc_debug("W:Fail to insert the chunk!\n");
 		return;
 	}
+
+	alloc_debug("I:Free %p\n", ptr);
+
+#if ALLOC_DEBUG == 2
+	for (int i = 0; i < CHUNK_QTY; ++i)
+	{
+		if (freed[i].size == 0) break;
+		printf("[%p,%X]", freed[i].start, freed[i].size);
+	}
+	printf("\n");
+#endif
 }
 
 void *malloc(size_t size)
 {
 	if (size == 0) {
-		// printf("You cannot allocate 0 bytes of memory!\n");
+		alloc_debug("E:Try to alloc 0 Bytes\n");
 		return NULL;
 	}
 
 	_chunk_t chunk;
 	if (remove_chunk_by_size(&chunk, freed, size) == 0)
 	{
-		// printf("Heap is full\n");
+		alloc_debug("E:Heap is full\n");
 		return NULL;
 	}
 
 	if (insert_chunk(&chunk, allocated) == 0)
 	{
-		// printf("Fail to insert the chunk!\n");
+		alloc_debug("E:Fail to insert chunk\n");
 		return NULL;
 	}
+	alloc_debug("I:Alloc %p\n", chunk.start);
 
+#if ALLOC_DEBUG == 2
+	for (int i = 0; i < CHUNK_QTY; ++i)
+	{
+		if (freed[i].size == 0) break;
+		printf("[%p,%X]", freed[i].start, freed[i].size);
+	}
+	printf("\n");
+#endif
 	return chunk.start;
 }
